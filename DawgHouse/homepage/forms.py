830903d82cc4cefdefbf25a7737bfc79baff5d2e
j-neeley <jonahneeley12@yahoo.com>
2023-10-13 21:32:24 -0500

    Added check for if two password fields match

--- DawgHouse/homepage/forms.py
+++ DawgHouse/homepage/forms.py
@@ -4,7 +4,11 @@ from django.core.exceptions import ValidationError
 from .models import DawgHouseUser
 
 class CustomUserCreationForm(forms.ModelForm):
+    error_messages = {
+        'password_mismatch': "The two password fields did not match."
+    }
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
+    retype_password = forms.CharField(label='Retype Password', widget=forms.PasswordInput, error_messages={'required': 'The two password fields did not match.'})
 
     class Meta:
         model = DawgHouseUser
@@ -15,13 +19,21 @@ class CustomUserCreationForm(forms.ModelForm):
             },
         }
 
-    def clean_password(self):
-        password = self.cleaned_data.get('password')
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get('password')
+        retype_password = cleaned_data.get('retype_password')
+
+        if password and retype_password:
+            if password != retype_password:
+                self.add_error('retype_password', self.error_messages['password_mismatch'])
+
         try:
             validate_password(password, self.instance)
         except ValidationError as error:
             raise forms.ValidationError(list(error.messages))
-        return password
+
+        return cleaned_data
 
     def save(self, commit=True):
         user = super(CustomUserCreationForm, self).save(commit=False)
--- DawgHouse/homepage/views.py
+++ DawgHouse/homepage/views.py
@@ -14,10 +14,15 @@ def signupView(request):
         if form.is_valid():
             user = form.save()
             messages.success(request, 'Account created successfully')
-            return HttpResponseRedirect('/')
+            # for these redirects, put the URL path in there
+            # for example, if we wanted to go to signup with would be "signup/"
+            # for now upon successful recreation we will redirect home, which is path ""
+            # we can change this later
+            return redirect('/')
         else:
-            for message in form.errors.values():
-                messages.error(request, message[0])
+            for field in form:
+                for error in field.errors:
+                    messages.error(request, f"{field.label}: {error}")
             return redirect('signup')
     else:
         form = CustomUserCreationForm()
