2c0c24ef52a8638627b4e7714beb27043cb6b6cb
j-neeley <jonahneeley12@yahoo.com>
2023-10-17 21:12:15 -0500

    Added login functionality

--- DawgHouse/homepage/urls.py
+++ DawgHouse/homepage/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.login, name="login"),
+    path("", views.home_view, name="home_view"),
+    path("login/", views.login_view, name="login_view"),
     path("signup/", views.signupView, name="signup"),
+    path("logout/", views.logout_view, name="logout_view")
 ]
--- DawgHouse/homepage/views.py
+++ DawgHouse/homepage/views.py
@@ -1,12 +1,32 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .models import DawgHouseUser
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, LoginForm
 
-def login(request):
-    return render(request, "login.html")
+def home_view(request):
+    return render(request, 'homepage.html')
+
+def login_view(request):
+    form = LoginForm()
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate (
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password'],
+            )
+        if user is not None:
+            login(request, user)
+            #right now this redirects to a landing page but this will need to be changed to the user's timeline later
+            return redirect('/')
+        else:
+            message = 'Incorrect Dawtag or Password'
+
+        return render(request, "login.html", {'form': form, 'message': message})
+        
+    return render(request, "login.html", {'form': form})
 
 def signupView(request):
     if request.method == 'POST':
@@ -23,9 +43,13 @@ def signupView(request):
             for field in form:
                 for error in field.errors:
                     messages.error(request, f"{field.label}: {error}")
-            return redirect('signup')
+            return redirect('/signup/')
     else:
         form = CustomUserCreationForm()
 
     return render(request, 'signup.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
 
